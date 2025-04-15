d7ae69325a78d3cfe1322d2fcfcf0eb0af743e03
FADRIQUE_MOLINA\josem <joeydicastro@gmail.com>
2025-04-15 12:51:57 -0700

    Adding home page screen to start customizing. Adding linter rules to prefer constants.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/presentation/screens/home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: HomePage(),
     );
   }
 }
